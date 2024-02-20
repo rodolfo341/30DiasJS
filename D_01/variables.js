4651bb736d7bd0ce6ff1bba40e4918746a91dad4
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-02-20 16:04:56 -0300

    dia 1 ej 1

--- D_01/variables.js
+++ D_01/variables.js
@@ -1,11 +1,19 @@
-let cadena = "Hola mundo";
+let cadena;
 console.log(cadena);
 
-let bandera = false;
+let bandera;
 console.log(bandera);
 
 let indefinido;
 console.log(indefinido);
 
-let nulo = null;
+let nulo;
 console.log(nulo);
+/*Comprobando los tipos de datos
+Para comprobar el tipo de datos de un determinado valor, utilizamos el operador 'typeof'. Véase el siguiente ejemplo.
+
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // número
+console.log(typeof true ) // boolean
+console.log(typeof null) // tipo de objeto
+console.log(typeof undefined) // undefined*/
