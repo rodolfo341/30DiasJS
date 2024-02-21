0b9bc7b787400db3893133316219fdbe0c7d8b72
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-02-21 08:32:57 -0300

    30 dias js dia-02 ej-11

--- D_02/app.js
+++ D_02/app.js
@@ -30,6 +30,9 @@ console.log(desafio.split());
 console.log(desafio.split(" "));
 
 // 11 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' divide la cadena en la coma y cámbiala a una matriz.
+let empresas = "'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'";
+console.log(empresas.split(","));
+
 // 12 Cambie 30 días de JavaScript a 30 días de Python usando el método replace().
 // 13 ¿Qué es el carácter en el índice 15 en la cadena '30 días de JavaScript'? Utilice el método charAt().
 // 14 ¿Cuál es el código de carácter de J en la cadena '30 días de JavaScript' usando charCodeAt()
