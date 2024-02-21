5f5b004763ea7c75b208d51937fcade5d369cfba
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-02-21 20:18:44 -0300

    30 dias js dia-02 nivel-2 ej-02

--- D_02/app2.js
+++ D_02/app2.js
@@ -1,5 +1,11 @@
 // 01 Usando console.log() imprima la siguiente declaración:
-// The quote 'There is no exercise better for the heart than reaching down and lifting people up.' 
-// by John Holmes teaches us to help one another.
+//    The quote 'There is no exercise better for the heart than reaching down and lifting people up.' 
+//    by John Holmes teaches us to help one another.
 console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' ");
 console.log("by John Holmes teaches us to help one another.");
+
+// 02 Usando console.log() imprima la siguiente cita de la Madre Teresa:
+//    "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- 
+//     with charity you give love, so don't just give money but reach out your hand instead."
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same");
+console.log(" -- with charity you give love, so don't just give money but reach out your hand instead.");
