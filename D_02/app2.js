d0d2ce9f5de5abe1ed3df1b240daffa7acfb8566
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-02-21 20:11:46 -0300

    30 dias js dia-02 nivel-2 ej-01

---
+++ D_02/app2.js
@@ -0,0 +1,5 @@
+// 01 Usando console.log() imprima la siguiente declaración:
+// The quote 'There is no exercise better for the heart than reaching down and lifting people up.' 
+// by John Holmes teaches us to help one another.
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' ");
+console.log("by John Holmes teaches us to help one another.");
