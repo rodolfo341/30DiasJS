f336e0d58096f1769298d64115ff23af8671b320
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-02-21 22:28:46 -0300

    30 dias js dia-02 nivel-2 ej-04

--- D_02/app2.js
+++ D_02/app2.js
@@ -9,3 +9,38 @@ console.log("by John Holmes teaches us to help one another.");
 //     with charity you give love, so don't just give money but reach out your hand instead."
 console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same");
 console.log(" -- with charity you give love, so don't just give money but reach out your hand instead.");
+
+// 03 Compruebe si typeof '10' es exactamente igual a 10. Si no, hágalo exactamente igual.
+console.log(typeof('10'));
+console.log(typeof(10));
+
+// 04 Compruebe si parseFloat('9.8') es igual a 10, si no, hágalo exactamente igual a 10.
+let num = '9.8';
+let numPF = parseFloat(num);
+console.log(10);
+console.log(numPF);
+
+// 05 Verifique si 'on' se encuentra tanto en Python como en la jerga
+
+// 06 Espero que este curso no esté lleno de jerga. Compruebe si jargon está en la oración.
+
+// 07 Genere un número aleatorio entre 0 y 100 inclusive.
+
+// 08 Genere un número aleatorio entre 50 y 100 inclusive.
+
+// 09 Genere un número aleatorio entre 0 y 255 inclusive.
+
+// 10 Acceda a los caracteres de la cadena 'JavaScript' utilizando un número aleatorio.
+
+// 11 Use console.log() y caracteres de escape para imprimir el siguiente patrón.
+//        1 1 1 1 1
+//        2 1 2 4 8
+//        3 1 3 9 27
+//        4 1 4 16 64
+//        5 1 5 25 125
+
+// 12 Usa substr para separar la frase porque porque porque de la siguiente oración:
+//    'No puedes terminar una oración con porque porque porque es una conjunción'
+
+
+
