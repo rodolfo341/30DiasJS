daab7c2f2156b8abff4d1e5045d4e2e8bf046b2f
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-02-27 03:00:51 -0300

    30 dias js dia 3 nivel 1 ejercicio 1

---
+++ D_03/app.js
@@ -0,0 +1,10 @@
+//  01  Declare las siguientes variables; 
+//     firstName, 
+//     lastName, 
+//     country, 
+//     city, 
+//     age, 
+//     isMarried, 
+//     year 
+// y asignar un valor, 
+// use el operador typeof para verificar diferentes tipos de datos.
