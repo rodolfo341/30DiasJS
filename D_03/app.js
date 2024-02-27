67142ea7edf2e74a7a86fdb080b6ec3c832f2578
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-02-27 03:12:09 -0300

    30 dias js dia 3 nivel 1 ejercicio 1 terminado

--- D_03/app.js
+++ D_03/app.js
@@ -8,3 +8,19 @@
 //     year 
 // y asignar un valor, 
 // use el operador typeof para verificar diferentes tipos de datos.
+
+let firstName = "Rodolfo"
+let lastName = "Aravena"
+let country = "Chile"
+let city = "Santiago"
+let age = 52
+let isMarried = true 
+let year = 2024 
+
+console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof country)
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof isMarried)
+console.log(typeof year)
