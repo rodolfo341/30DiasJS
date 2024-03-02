d6f34fee47344d528adb4191da6589b29fdccedd
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-03-02 19:54:03 -0300

    30 dias JS dia 04 nivel 01 ej 01 empezado

---
+++ D_04/app.js
@@ -0,0 +1,5 @@
+// 01 Obtenga la entrada del usuario usando el aviso ("Ingrese su edad:"). 
+//      Si el usuario tiene 18 años o más, muestre el mensaje: 
+//      'Tiene la edad suficiente para conducir', 
+//      pero si no tiene 18 años, brinde otro mensaje que indique 
+//      que debe esperar la cantidad de años que necesita para cumplir 18.
