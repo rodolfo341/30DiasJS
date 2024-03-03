a04548e3e0a0333822f74d5e226fc00511be4ee2
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-03-03 19:44:16 -0300

    30 dias js dia 04 nivel 01 ej 02 empezado

--- D_04/app.js
+++ D_04/app.js
@@ -4,9 +4,15 @@
 //      pero si no tiene 18 años, brinde otro mensaje que indique 
 //      que debe esperar la cantidad de años que necesita para cumplir 18.
 
-let edad = prompt('ingrese edad')
-if ( edad >= 18 ) {
-    console.log('Tiene la edad suficiente para conducir')
-} else {
-    console.log('debe esperar ' + ( 18 - edad ) + ' para cumplir 18')
-}
+// let edad = prompt('ingrese edad')
+// if ( edad >= 18 ) {
+//     console.log('Tiene la edad suficiente para conducir')
+// } else {
+//     console.log('debe esperar ' + ( 18 - edad ) + ' para cumplir 18')
+// }
+
+// 02 Compara los valores de myAge y yourAge usando if... else. 
+// Según la comparación, registre el resultado en la consola 
+// indicando quién es mayor (tú o yo). Utilice prompt(“Ingrese su edad:”) 
+// para obtener la edad como entrada.
+
