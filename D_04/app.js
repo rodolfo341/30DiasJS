b8af15c67f528df890fcabc073e570de0e72c8b8
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-04-21 13:56:23 -0400

    30 dias JS dia 04 ej nivel 02 ej 01

--- D_04/app.js
+++ D_04/app.js
@@ -54,19 +54,12 @@
 // 0-49, F
 
 let nota = parseInt(prompt('Nota: '))
-if ( nota < 0 || nota > 100){
-    console.log('Error!')
-} else if ( nota < 50 ) {
-    console.log('F')
-} else if ( nota < 60 ) {
-    console.log('D')
-} else if ( nota < 70 ) {
-    console.log('C')
-} else if ( nota < 90 ) {
-    console.log('B')
-} else if ( nota <= 100 ) {
-    console.log('A')
-}
+if ( nota < 0 || nota > 100){ console.log('Error!') } 
+else if ( nota < 50 ) { console.log('F') } 
+else if ( nota < 60 ) { console.log('D') } 
+else if ( nota < 70 ) { console.log('C') } 
+else if ( nota < 90 ) { console.log('B') } 
+else if ( nota <= 100 ) { console.log('A') }
 
 
 
