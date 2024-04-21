9a85cb3bf912fef292122488a27c15e3f7a16ce0
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-04-21 14:11:54 -0400

    30 dias JS dia 04 ej nivel 02 ej 02

--- D_04/app.js
+++ D_04/app.js
@@ -46,21 +46,38 @@
 //     console.log("No es par")
 // }
 
-// nivel 01 ej 01 Escriba un código que pueda calificar a los estudiantes de acuerdo con sus puntajes:
+// nivel 02 ej 01 Escriba un código que pueda calificar a los estudiantes de acuerdo con sus puntajes:
 // 80-100, A
 // 70-89, B
 // 60-69, C
 // 50-59, D
 // 0-49, F
 
-let nota = parseInt(prompt('Nota: '))
-if ( nota < 0 || nota > 100){ console.log('Error!') } 
-else if ( nota < 50 ) { console.log('F') } 
-else if ( nota < 60 ) { console.log('D') } 
-else if ( nota < 70 ) { console.log('C') } 
-else if ( nota < 90 ) { console.log('B') } 
-else if ( nota <= 100 ) { console.log('A') }
+// let nota = parseInt(prompt('Nota: '))
+// if ( nota < 0 || nota > 100) { console.log('Error!') } 
+// else if ( nota < 50 ) { console.log('F') } 
+// else if ( nota < 60 ) { console.log('D') } 
+// else if ( nota < 70 ) { console.log('C') } 
+// else if ( nota < 90 ) { console.log('B') } 
+// else if ( nota <= 100 ) { console.log('A') }
 
+// nivel 02 ej 02 Consulta si la temporada es Otoño, Invierno, Primavera o Verano. 
+// Si la entrada del usuario es :
+// Septiembre, Octubre o Noviembre, la temporada es Otoño.
+// Diciembre, Enero o Febrero, la temporada es Invierno.
+// Marzo, Abril o Mayo, la temporada es Primavera
+// Junio, Julio o Agosto, la temporada es Verano
 
+let mes = parseInt(prompt('ingrese mes \n 1) Septiembre, Octubre o Noviembre \n 2) Diciembre, Enero o Febrero \n 3) Marzo, Abril o Mayo \n 4) Junio, Julio o Agosto'));
 
-
+if ( mes == 1 ) {
+    console.log('la temporada es Otoño.')
+} else if ( mes == 2 ) {
+    console.log('la temporada es Invierno.')
+} else if ( mes == 3 ) {
+    console.log('la temporada es Primavera.')
+} else if ( mes == 4 ) {
+    console.log('la temporada es Veranp.')
+} else {
+    console.log('Error!')
+}
