e8901dd9ebf1bf1dbe1394e56f49f14ec4a34732
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-04-25 18:38:18 -0400

    30 dias JS dia 05 Nivel 01 ej 05

--- D_05/app.js
+++ D_05/app.js
@@ -2,14 +2,19 @@
 // const arreglo1 = array()
 // const arreglo2 = []
 
-// Nivel 1 aj 02 Declara un array com mas de 5 elementos.
-const arreglo = [1,2,3,4,5,6]
+// Nivel 01 aj 02 Declara un array com mas de 5 elementos.
+// const arreglo = [1,2,3,4,5,6]
 
 // Nivel 1 ej 03 Encuentra la longitud de tu array.
-console.log(arreglo.length)
+// console.log(arreglo.length)
 
 // Nivel 01 ej o4 Obtenga el primer elemento, el elemento del medio y el último elemento de un array.
-console.log(arreglo[0])
-console.log(arreglo[arreglo.length/2])
-console.log(arreglo[arreglo.length-1])
+// console.log(arreglo[0])
+// console.log(arreglo[arreglo.length/2])
+// console.log(arreglo[arreglo.length-1])
 
+// Nivel 01 ej 05 Declara un array llamado mixedDataTypes, 
+// coloque diferentes tipos de datos en el array y encuentre la longitud del array. 
+// El tamaño del array debe ser mayor que 5.
+const mixedDataTypes =[5,"hola", true, 3, 3.5, "chao"]
+console.log(mixedDataTypes.length)
