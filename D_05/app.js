bc831f2d4f935c47bf97b678a485de66ae3f645e
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-04-27 17:34:59 -0400

    30 dias JS dia 05 nivel 01 ej 12

--- D_05/app.js
+++ D_05/app.js
@@ -41,9 +41,11 @@ for ( let i = 0; i < itCompanies.length; i++){
 }
 
 // Nivel 01 ej 11 Cambie el nombre de cada empresa a mayúsculas uno por uno e imprímalos.
-for ( let i = 0; i < itCompanies.length; i++){
-    itCompanies[i] = itCompanies[i].toUpperCase()
-}
+// for ( let i = 0; i < itCompanies.length; i++){
+//     itCompanies[i] = itCompanies[i].toUpperCase()
+// }
 console.log(itCompanies)
 
-
+// Nivel 01 ej 12 Imprime el array como una oración: 
+// Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon son grandes empresas de TI.
+console.log(itCompanies[0] + ', ' + itCompanies[1] + ', ' + itCompanies[2] + ', ' + itCompanies[3] + ', ' + itCompanies[4] + ', ' + itCompanies[5] + ', ' + itCompanies[6] + ' y ' + itCompanies[7] + 'son grandes empresas IT ')
