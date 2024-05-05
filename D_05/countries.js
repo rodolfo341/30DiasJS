f8ab280090ce0cbab8f01e380b533fa60e43f2f7
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-05 15:49:54 -0400

    30 dias JS dia 05 nivel 02 ej 04

--- D_05/countries.js
+++ D_05/countries.js
@@ -11,3 +11,8 @@ const countries = [
     "Japan",
     "Kenya",
   ];
+
+  // ej 04 verifique si 'Ethiopia' existe en el array si existe, imprima 'ETHIOPIA'. 
+  // Si no existe agregar a la lista de países.
+
+  
