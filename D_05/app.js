f5da650fd8c89052eaaef6e9d39b89a442657c54
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-05 12:33:50 -0400

    30 dias JS dia 05 nivel 02

--- D_05/app.js
+++ D_05/app.js
@@ -22,71 +22,71 @@
 // Nivel 01 ej 06 Declare un variable array de nombre itCompanies y 
 // asignarles valores iniciales 
 // Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon.
-const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+// const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
 
-// Nivel 01 ej 07 Imprima el array usando console.log().
-console.log(itCompanies)
+// // Nivel 01 ej 07 Imprima el array usando console.log().
+// console.log(itCompanies)
 
-// Nivel 01 ej 08 Imprima el número de empresas en el array.
-console.log(itCompanies.length)
+// // Nivel 01 ej 08 Imprima el número de empresas en el array.
+// console.log(itCompanies.length)
 
-// Nivel 01 ej 09 Imprima el número de empresas en el array.Imprime la primer empresa , la intermedia y la última empresa
-console.log(itCompanies[0])
-console.log(itCompanies[ Math.trunc(itCompanies.length / 2 )])
-console.log(itCompanies[itCompanies.length - 1])
+// // Nivel 01 ej 09 Imprima el número de empresas en el array.Imprime la primer empresa , la intermedia y la última empresa
+// console.log(itCompanies[0])
+// console.log(itCompanies[ Math.trunc(itCompanies.length / 2 )])
+// console.log(itCompanies[itCompanies.length - 1])
 
-// Nivel 01 ej 10 Imprime cada empresa.
-for ( let i = 0; i < itCompanies.length; i++){
-    console.log(itCompanies[i])
-}
-
-// Nivel 01 ej 11 Cambie el nombre de cada empresa a mayúsculas uno por uno e imprímalos.
+// // Nivel 01 ej 10 Imprime cada empresa.
 // for ( let i = 0; i < itCompanies.length; i++){
-//     itCompanies[i] = itCompanies[i].toUpperCase()
+//     console.log(itCompanies[i])
 // }
-console.log(itCompanies)
 
-// Nivel 01 ej 12 Imprime el array como una oración: 
-// Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon son grandes empresas de TI.
-console.log(itCompanies[0] + ', ' + itCompanies[1] + ', ' + itCompanies[2] + ', ' + itCompanies[3] + ', ' + itCompanies[4] + ', ' + itCompanies[5] + ', ' + itCompanies[6] + ' y ' + itCompanies[7] + 'son grandes empresas IT ')
+// // Nivel 01 ej 11 Cambie el nombre de cada empresa a mayúsculas uno por uno e imprímalos.
+// // for ( let i = 0; i < itCompanies.length; i++){
+// //     itCompanies[i] = itCompanies[i].toUpperCase()
+// // }
+// console.log(itCompanies)
 
-// Nivel 01 ej 13 Compruebe si existe una determinada empresa en el array itCompanies. 
-// Si existe, retorna la empresa; de lo contrario, retorna la empresa no existe
-let bandera = false
-for ( let i = 0; i < itCompanies.length ; i++ ){
-    if ( itCompanies[i] == 'Apple' ){
-        console.log(itCompanies[i])
-        bandera = true
-    }
-}
-if ( bandera == false ) {
-    console.log('la empresa no existe')
-}
+// // Nivel 01 ej 12 Imprime el array como una oración: 
+// // Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon son grandes empresas de TI.
+// console.log(itCompanies[0] + ', ' + itCompanies[1] + ', ' + itCompanies[2] + ', ' + itCompanies[3] + ', ' + itCompanies[4] + ', ' + itCompanies[5] + ', ' + itCompanies[6] + ' y ' + itCompanies[7] + 'son grandes empresas IT ')
 
-// Nivel 01 ej 14 Filtre las empresas que tienen más de una 'o' sin el método filter() 
-let itCompaniesFiltradas = [];
+// // Nivel 01 ej 13 Compruebe si existe una determinada empresa en el array itCompanies. 
+// // Si existe, retorna la empresa; de lo contrario, retorna la empresa no existe
+// let bandera = false
+// for ( let i = 0; i < itCompanies.length ; i++ ){
+//     if ( itCompanies[i] == 'Apple' ){
+//         console.log(itCompanies[i])
+//         bandera = true
+//     }
+// }
+// if ( bandera == false ) {
+//     console.log('la empresa no existe')
+// }
 
-for (var i = 0; i < itCompanies.length; i++) {
-  var count = 0;
-  for (var j = 0; j < itCompanies[i].length; j++) {
-    if (itCompanies[i][j] === 'o' || itCompanies[i][j] === 'O') {
-      count++;
-    }
-  }
-  if (count > 1) {
-    itCompaniesFiltradas.push(itCompanies[i]);
-  }
-}
+// // Nivel 01 ej 14 Filtre las empresas que tienen más de una 'o' sin el método filter() 
+// let itCompaniesFiltradas = [];
 
-console.log(itCompaniesFiltradas);
+// for (var i = 0; i < itCompanies.length; i++) {
+//   var count = 0;
+//   for (var j = 0; j < itCompanies[i].length; j++) {
+//     if (itCompanies[i][j] === 'o' || itCompanies[i][j] === 'O') {
+//       count++;
+//     }
+//   }
+//   if (count > 1) {
+//     itCompaniesFiltradas.push(itCompanies[i]);
+//   }
+// }
 
-// Nivel 01 ej 15 Ordene el array usando el método sort()
-itCompanies.sort()
-console.log(itCompanies) 
+// console.log(itCompaniesFiltradas);
 
-// Nivel 01 ej 16 Invierte la array usando el método reverse()
-itCompanies.reverse()
-console.log(itCompanies)
+// // Nivel 01 ej 15 Ordene el array usando el método sort()
+// itCompanies.sort()
+// console.log(itCompanies) 
+
+// // Nivel 01 ej 16 Invierte la array usando el método reverse()
+// itCompanies.reverse()
+// console.log(itCompanies)
 
 // Nivel 01 ej 17 Cortar las primeras 3 empresas del array
 // Nivel 01 ej 18 Cortar las últimas 3 empresas del array
@@ -95,6 +95,9 @@ console.log(itCompanies)
 // Nivel 01 ej 21 Eliminar la empresa o empresas intermedias de TI del array
 // Nivel 01 ej 22 Elimine la última empresa de TI del array
 // Nivel 01 ej 23 Eliminar todas las empresas de TI
-itCompanies.splice(0,itCompanies.length)
-console.log(itCompanies)
+// itCompanies.splice(0,itCompanies.length)
+// console.log(itCompanies)
+
+// Nivel 02
+
 
