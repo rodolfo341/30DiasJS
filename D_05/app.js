6446369cb4de686842fdd7458e87d95b4a9a6057
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-05 14:47:02 -0400

    30 dias JS dia 05 nivel 02 ej 01

--- D_05/app.js
+++ D_05/app.js
@@ -99,5 +99,9 @@
 // console.log(itCompanies)
 
 // Nivel 02
-
+// ej 01 Cree un archivo de countries.js separado y 
+// almacene el array de países en este archivo, 
+// cree un archivo separado web_techs.js y 
+// almacene el array de webTechs en este archivo. 
+// Acceda a ambos archivos en el archivo main.js
 
