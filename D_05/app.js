1afbf8d4cdb13fa56be5e7d73542b2bcc6f09fd1
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-05 15:47:55 -0400

    30 dias JS dia 05 nivel 02 ej 03

--- D_05/app.js
+++ D_05/app.js
@@ -135,5 +135,5 @@ const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
 shoppingCart.unshift('Meat')
 
 shoppingCart.push('Sugar')
-shoppingCart.splice(4,1)
+shoppingCart[3] = 'Green Tea'
 console.log(shoppingCart)
