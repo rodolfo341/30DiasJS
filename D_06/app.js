bc983e75b3f39a27ee8cbaef116ee3a1616400e3
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-07 15:49:29 -0400

    30 dias js dia 06 ej 01

---
+++ D_06/app.js
@@ -0,0 +1,27 @@
+const countries = [
+    "Albania",
+    "Bolivia",
+    "Canada",
+    "Denmark",
+    "Ethiopia",
+    "Finland",
+    "Germany",
+    "Hungary",
+    "Ireland",
+    "Japan",
+    "Kenya",
+  ];
+  
+  const webTechs = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Redux",
+    "Node",
+    "MongoDB",
+  ];
+  
+  const mernStack = ["MongoDB", "Express", "React", "Node"];
+
+  // Itera de 0 a 10 usando el bucle for, haga lo mismo usando los bucles while y do while
