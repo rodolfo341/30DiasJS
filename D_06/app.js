878fcd5c08802f8e9ff33d998b3011e77e684707
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-09 19:07:48 -0400

    practica blueweb

--- D_06/app.js
+++ D_06/app.js
@@ -1,49 +1,6 @@
-const countries = [
-    "Albania",
-    "Bolivia",
-    "Canada",
-    "Denmark",
-    "Ethiopia",
-    "Finland",
-    "Germany",
-    "Hungary",
-    "Ireland",
-    "Japan",
-    "Kenya",
-  ];
-  
-  const webTechs = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React",
-    "Redux",
-    "Node",
-    "MongoDB",
-  ];
-  
-  const mernStack = ["MongoDB", "Express", "React", "Node"];
 
-  // Itera de 0 a 10 usando el bucle for, haga lo mismo usando los bucles while y do while
-  // 06 01 02 Itera 10 to 0 usando el bucle for, haga lo mismo usando los bucles while y do while
-// 03 Itera de 0 a n usando el bucle for
-let n = prompt('N: ')
-  for ( let i=0 ; i<n ; i++){
-    console.log(countries[i])
-  }
+for(let i=0;i<=10;i++){
+  let num = Math.floor(Math.random() * (10 - 1)) + 1;
+  console.log(num)
+}
 
-  let j = 0
-  while( j < 10 ){
-    console.log(countries[j])
-    j++
-  }
-
-  let z = 0
-  do {
-    console.log(countries[z])
-    z++
-  } while (z < 10);
-
-
-
-  
