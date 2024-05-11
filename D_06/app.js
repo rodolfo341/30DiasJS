b558de0180ad7eb40954fe19ca5a5e5796c3c46d
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-11 18:02:10 -0400

    practica blueweb

--- D_06/app.js
+++ D_06/app.js
@@ -1,6 +1,5 @@
-
-for(let i=0;i<=10;i++){
-  let num = Math.floor(Math.random() * (10 - 1)) + 1;
-  console.log(num)
+function saludar(nombre) {
+  console.log("hola " + nombre)
 }
 
+saludar('Manchas')
