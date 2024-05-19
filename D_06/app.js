d8cba212bb55c5a56f0acf8e1b7c75c2bd57d916
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-19 15:25:43 -0400

    30 dias js dia 6 nivel 1 ej 6

--- D_06/app.js
+++ D_06/app.js
@@ -1,6 +1,20 @@
+// Usando un bucle imprime el siguiente patrón:
 
-for ( let i = 0 ; i<=10 ; i++ ) {
-    console.log(`${i} x ${i} = ${i * i}`)
+// i    i^2   i^3
+// 0    0     0
+// 1    1     1
+// 2    4     8
+// 3    9     27
+// 4    16    64
+// 5    25    125
+// 6    36    216
+// 7    49    343
+// 8    64    512
+// 9    81    729
+// 10   100   1000
+
+console.log(' i    i^2   i^3')
+
+for(let i = 0; i<=10 ; i++){
+    console.log(`${i}  ${i**2} ${i**3}`)
 }
-
-
