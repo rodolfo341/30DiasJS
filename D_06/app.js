ce619514ee4d365a02e9e868aba21a2e7cd3eed1
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-19 17:12:32 -0400

    30 dias js dia 6 nivel 1 ej 10

--- D_06/app.js
+++ D_06/app.js
@@ -1,15 +1,6 @@
-for (let i = 2; i <= 100; i++) {
-    let esPrimo = true;
-  
-    for (let j = 2; j <= Math.sqrt(i); j++) {
-      if (i % j === 0) {
-        esPrimo = false;
-        break;
-      }
-    }
-  
-    if (esPrimo) {
-      console.log(i);
-    }
+let acum = 0
+for ( let i=0 ; i <=100 ; i++ ) {
+    acum += i
 }
+console.log(acum)
   
