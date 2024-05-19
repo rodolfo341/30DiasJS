70a15f67faa711ae4dde3e2d152ccdfdb776e145
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-19 19:38:51 -0400

    30 dias js dia 6 nivel 1 ej 12

--- D_06/app.js
+++ D_06/app.js
@@ -1,8 +1,13 @@
 
+// Usa el bucle para iterar de 0 a 100 e imprimir la suma de todos los pares y la suma de todos los impares. Imprimir suma de pares y suma de impares como un array
+
 let acum = 0
+let acum2 = 0
 for ( let i=0 ; i <=100 ; i++ ) {
-    if ( i%2 !== 0 ){
+    if ( i%2 === 0 ){
         acum += i
+    } else {
+        acum2 += i
     }
 }
-console.log(acum)
+console.log([acum,acum2])
