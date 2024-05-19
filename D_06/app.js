fce66a9390d9595ec567a616f593f70c26032070
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-19 16:59:15 -0400

    30 dias js dia 6 nivel 1 ej 7

--- D_06/app.js
+++ D_06/app.js
@@ -1,20 +1,7 @@
-// Usando un bucle imprime el siguiente patrón:
+// Usa el bucle for para iterar de 0 a 100 e imprima solo números pares
 
-// i    i^2   i^3
-// 0    0     0
-// 1    1     1
-// 2    4     8
-// 3    9     27
-// 4    16    64
-// 5    25    125
-// 6    36    216
-// 7    49    343
-// 8    64    512
-// 9    81    729
-// 10   100   1000
-
-console.log(' i    i^2   i^3')
-
-for(let i = 0; i<=10 ; i++){
-    console.log(`${i}  ${i**2} ${i**3}`)
+for ( let i=0 ; i <=100 ; i++ ) {
+    if ( i % 2 === 0 ){
+        console.log( i )
+    }
 }
