a7741296da94c86cec676db763fdc8017e6d7e7b
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-21 14:35:20 -0400

    30 dias js dia 7 ej 15

--- D_07/app.js
+++ D_07/app.js
@@ -1,15 +1,6 @@
-function max(x,y,z){
-    if ( x>y && x>z ){
-        return x
-    } else if ( y>x && y>z ){
-        return y
-    } else {
-        return z
-    }
+let persona = {
+    nombre: 'rodolfo',
+    edad: 52
 }
 
-const x = parseInt(prompt('x'))
-const y = parseInt(prompt('y'))
-const z = parseInt(prompt('x'))
-
-console.log(`el mayor es ${max(x,y,z)}`)
+console.log(persona.nombre)
