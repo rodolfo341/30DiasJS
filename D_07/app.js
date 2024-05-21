bb926d24411a444b429323e5eeaee7d21c868b71
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2024-05-21 12:33:17 -0400

    30 dias js dia 7 ej 14

--- D_07/app.js
+++ D_07/app.js
@@ -1,4 +1,13 @@
-function densidad(masa,volumen){
-    return masa / volumen
+function estacion(mes){
+    if  (mes === 1 || mes === 2 || mes === 12 ){
+        console.log('verano')
+    } else if  (mes === 3 || mes === 4 || mes === 5 ){
+        console.log('otoño')
+    } else if  (mes === 6 || mes === 7 || mes === 8 ){
+        console.log('invierno')
+    } else if  (mes === 9 || mes === 10 || mes === 11 ){
+        console.log('primavera')
+    }
 }
-console.log(densidad(2,3))
+const mes = parseInt(prompt("ingrese mes"))
+estacion(mes)
