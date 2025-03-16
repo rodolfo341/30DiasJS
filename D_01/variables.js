671f551284c324afacf4fcc03ef5380b627acf20
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2025-03-16 17:27:11 -0300

    dia 1

--- D_01/variables.js
+++ D_01/variables.js
@@ -1,5 +1,30 @@
+// los comentarios pueden hacer que el código sea legible
 
-/*let nombre = "Rodolfo", apellido = "Aravena", esta_casado = false, pais = "Chile", edad = 52;
+// Bienvenido a 30DaysOfJavaScript
+
+/*Los comentarios pueden hacer el código legible, 
+fácil de usar e informativo*/
+
+let cadena = "cadena";
+
+let bool = true;
+
+let indefinido = undefined;
+
+console.log(typeof(cadena));
+
+console.log(typeof(bool));
+
+console.log(typeof(indefinido));
+
+/*
+let nombre = "Rodolfo";
+let apellido = "Aravena"
+let pais = "Chile"
+let edad = 52;
+let esta_casado = false
+*/
+let nombre = "Rodolfo", apellido = "Aravena", esta_casado = false, pais = "Chile", miEdad = 52, tuEdad = 32;
 
 console.log(nombre);
 
@@ -9,7 +34,7 @@ console.log(esta_casado);
 
 console.log(pais);
 
-console.log(edad);
+console.log("tengo " + miEdad + " años");
 
-*/
-// Bienvenido a 30DaysOfJavaScript
+console.log("Tú tienes " + tuEdad + " años")
+
