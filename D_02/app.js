dd3b8980c970cd348948e3576ba2cc828e3b3a48
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2025-03-17 10:28:21 -0300

    empezar dia dia 2

--- D_02/app.js
+++ D_02/app.js
@@ -3,45 +3,73 @@
 let desafio = "30 dias de JavaScript";
 
 // 02 Imprima la cadena en la consola del navegador usando console.log()
-console.log(desafio);
+console.log("2.- uso de console.log(): " + desafio);
 
-// 03 Imprima la longitud 7de la cadena en la consola del navegador usando console.log()
-console.log("longitud de la ladena " + " \"" + desafio + "\"" + " es: " + desafio.length);
+// 03 Imprima la longitud de la cadena en la consola del navegador usando console.log()
+console.log("3.- longitud de la cadena con .length: " + " \"" + desafio + "\"" + " es: " + desafio.length);
 
 // 04 Cambie todos los caracteres de cadena a letras mayúsculas usando el método toUpperCase()
-console.log(desafio.toUpperCase());
+console.log("4.- todo en mayusculas con .toUpperCase(): " + desafio.toUpperCase());
 
 // 05 Cambie todos los caracteres de la cadena a letras minúsculas usando el método toLowerCase()
-console.log(desafio.toLowerCase());
+console.log(`5.- todo en minusculas con toLowerCase(): ${desafio.toLowerCase()}`); 
 
 // 06 Corta (segmenta) la primera palabra de la cadena usando el método substr() o substring()
-console.log(desafio.substring(0,2));
+console.log(`6.- primera palabra con sunstring(): ${desafio.substring(0,2)}`);
 
 // 07 Corta la frase Days Of JavaScript de 30 Days Of JavaScript.
-console.log(desafio.split(" "));
+console.log(`7.- convertir en array de cada cadena con .split(" ")${desafio.split(" ")}`);
 
 // 08 Verifique si la cadena contiene una palabra dias usando el método includes()
-console.log(desafio.includes("dias"));
+let palabras = desafio.split(/\s+/);
+let palabra = "dias";
+let encontrado = palabras.includes(palabra);
+
+if(encontrado){
+    console.log(`8.- la palabra ${palabra} se encuentra`);
+} else {
+    console.log(`8.- la palabra ${palabra} no se encuentra`);
+}
 
 // 09 Divide la cadena en un array usando el método split()
-console.log(desafio.split());
+console.log(`9.- cadena "desafio" duvidida en un array de palabras: ${desafio.split()}`);
 
 // 10 Divida la cadena 30 días de JavaScript en el espacio usando el método split()
-console.log(desafio.split(" "));
+let newDesafio = desafio.split(" ");
+console.log(`10.- cadena "desafio" dividida en un array usando espacio " ": ${newDesafio}`);
+newDesafio.forEach(item => {
+    console.log(`\t ${item}`)
+})
 
-// 11 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' divide la cadena en la coma y cámbiala a una matriz.
+// 11 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
+// divide la cadena en la coma y cámbiala a una matriz.
 let empresas = "'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'";
-console.log(empresas.split(","));
+console.log(`11.- la cadena ${empresas} `)
+let newEmpresas = empresas.split(","); 
+console.log(`ahora un array de empresas:`);
+
+newEmpresas.forEach(empresa => {
+    console.log(`\t ${empresa}`);
+})
+
 
 // 12 Cambie 30 días de JavaScript a 30 días de Python usando el método replace().
-console.log(desafio.replace("JS","Python"));
+console.log("12 Cambie 30 días de JavaScript a 30 días de Python usando el método replace().")
+let aRemplazar = "dias";
+let paraReemplazar = "semanas";
+newDesafio = desafio.replace(aRemplazar,paraReemplazar);
+console.log(`cadena desafio: ${desafio}`);
+console.log(`cadena newDesafio: ${newDesafio}`);
 
 // 13 ¿Cual es el carácter en el índice 12 en la cadena '30 días de JS'? Utilice el método charAt().
-console.log(desafio.charAt(12));
+console.log(`caracter en el indice 12:'${desafio.charAt(12)}'`);
 
 // 14 ¿Cuál es el código de carácter de J en la cadena '30 días de JavaScript' usando charCodeAt()
 console.log(desafio.charCodeAt(11));
 
+
+
+/*
 // 15 Use indexOf para determinar la posición de la primera aparición de a en 30 días de JavaScript
 console.log(desafio.indexOf("a"));
 
@@ -82,3 +110,4 @@ console.log(dias.concat(" ","JavaScript"));
 
 // 25 Use el método repeat() para imprimir 30 días de JavaScript 2 veces
 console.log(desafio.repeat(2));
+*/
