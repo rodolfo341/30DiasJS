61e6349d08d541e90e99bd31c7fb056b88b0c9a7
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2025-03-20 15:42:49 -0300

    fin  dia 2 nivel 1

--- D_02/app.js
+++ D_02/app.js
@@ -1,7 +1,7 @@
 
 // 01 Declare una variable llamada desafío y asígnele un valor inicial '30 días de JavaScript'.
 let desafio = "30 dias de JavaScript";
-/*
+
 // 02 Imprima la cadena en la consola del navegador usando console.log()
 console.log("2.- uso de console.log(): " + desafio);
 
@@ -62,56 +62,57 @@ console.log(`cadena desafio: ${desafio}`);
 console.log(`cadena newDesafio: ${newDesafio}`);
 
 // 13 ¿Cual es el carácter en el índice 12 en la cadena '30 días de JS'? Utilice el método charAt().
-console.log(`caracter en el indice 12:'${desafio.charAt(12)}'`);
+console.log(`13.- caracter en el indice 12:'${desafio.charAt(12)}'`);
 
 // 14 ¿Cuál es el código de carácter de J en la cadena '30 días de JavaScript' usando charCodeAt()
-console.log(`el unicode de "j" en la posición 11 es ${desafio.charCodeAt(11)}`);
+console.log(`14.- el unicode de "j" en la posición 11 es ${desafio.charCodeAt(11)}`);
 
 // 15 Use indexOf para determinar la posición de la primera aparición de a en 30 días de JavaScript
-console.log(desafio.indexOf("a"));
+console.log(`15.- La posicion de la primera letra 'a' usando indexOf(): ${desafio.indexOf("a")}`);
 
 // 16 Utilice lastIndexOf para determinar la posición de la última aparición de a en 30 días de JavaScript.
-console.log(`ultimo indice de la letra "a" el el string desaio: ${desafio.lastIndexOf("a")}`);
+console.log(`16.- ultimo indice de la letra "a" el el string desaio: ${desafio.lastIndexOf("a")}`);
 
 // 17 Usa indexOf para encontrar la posición de la primera aparición de la palabra porque en la siguiente oración:
 //   'No puedes terminar una oración con porque porque porque es una conjunción'
-*/
+
 let cadena = "No puedes terminar una oración con porque porque porque es una conjunción";
-/*
-console.log(`primera aparición de "porque" en la cadena: ${cadena.indexOf("porque")}`);
+
+console.log(`17.- primera aparición de "porque" en la cadena: ${cadena.indexOf("porque")}`);
 console.log("hola");
 
 // 18 Usa lastIndexOf para encontrar la posición de la última aparición de la palabra porque en la siguiente oración:
 //   'No puedes terminar una oración con porque porque porque es una conjunción'
-console.log(`Última aparición de "porque" en la cadena: ${cadena.lastIndexOf("porque")}`);
+console.log(`18.- Última aparición de "porque" en la cadena: ${cadena.lastIndexOf("porque")}`);
 
 // 19 Usa search para encontrar la posición de la primera aparición de la palabra porque en la siguiente oración:
 //   'No puedes terminar una oración con porque porque porque es una conjunción'
-console.log(cadena.search("porque"));
+console.log(`19 .- primera aparición de la palabra 'porque' en la cadena usando search(): ${cadena.search("porque")}`);
 
 // 20 Use trim() para eliminar cualquier espacio en blanco final al principio y al final de una cadena. Por ejemplo, 
 //   '30 días de JavaScript'.
 let desafio2 = " 30 dias de JavaScript ";
-console.log(desafio2);
-console.log(`uso dee strim() elimina espacios en blanco l principio y al final de la cadena ${desafio2.trim()}`);
+
+console.log(`20.- uso dee strim() elimina espacios en blanco l principio y al final de la cadena ${desafio2.trim()}`);
 
 // 21 Use el método startsWith() con la cadena 30 días de JavaScript y haga que el resultado sea verdadero
-console.log(`devuelve true ya que comienza en 30 (startsWith): ${desafio.startsWith(30)}`);
-*/
+console.log(`21.- devuelve true ya que comienza en 30 (startsWith): ${desafio.startsWith(30)}`);
 
 // 22 Use el método endsWith() con la cadena 30 días de JavaScript y haga que el resultado sea verdadero
-console.log(`devuelve true por que la última palabra es "JavaScripyt" (endsWitch): {desafio.endsWith("JavaScript")}`);
+console.log(`22.- devuelve true por que la última palabra es "JavaScripyt" (endsWitch): {desafio.endsWith("JavaScript")}`);
 
-
-
-/*
 // 23 Usa el método match() para encontrar todos los a en 30 días de JavaScript
-console.log(desafio.match("a"));
+console.log(`23.- en metodo match() encuentra todas las letras 'a': ${desafio.match(/a/g)}`);
 
 // 24 Use concat() y fusione '30 días de' y 'JavaScript' en una sola cadena, '30 días de JavaScript'
-let dias = "30 dias de";
-console.log(dias.concat(" ","JavaScript"));
+let cad1 = "30";
+let cad2 = "días"
+let cad3 = "de";
+let cad4 = "JavaScript";
+let cad12 = cad1.concat(" ".concat(cad2));
+let cad34 = cad3.concat(" ".concat(cad4));
+let cadena1234 = cad12.concat(cad34);
+console.log(`24.- concatenacion usando concat(): ${cadena1234}`);
 
 // 25 Use el método repeat() para imprimir 30 días de JavaScript 2 veces
-console.log(desafio.repeat(2));
-*/
+console.log(`25.- repetir 10 veces la cadena usando repeat(): ${desafio.repeat(10)}`);
